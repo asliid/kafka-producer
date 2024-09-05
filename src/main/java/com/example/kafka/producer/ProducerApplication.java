b4e9a91f90a32bec599d48c1a1369a811e11bcd0
aslidongez <a.dongez@hotmail.com>
2024-09-05 19:45:51 +0300

    kafka producer completed

--- src/main/java/com/example/kafka/producer/ProducerApplication.java
+++ src/main/java/com/example/kafka/producer/ProducerApplication.java
@@ -1,8 +1,11 @@
 package com.example.kafka.producer;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProducerApplication {
-    
+public static void main(String[] args) {
+    SpringApplication.run(ProducerApplication.class,args);
+    }
 }
